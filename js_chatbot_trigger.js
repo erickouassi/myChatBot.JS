9e632d74e1f6d68241e6ee79778ee56b26e3c9f6
Eric Kouassi <39168930+erickouassi@users.noreply.github.com>
2023-12-10 22:55:08 -0500

    Add initial trigger page data

---
+++ js_chatbot_trigger.js
@@ -0,0 +1,23 @@
+var trigger = [
+	//Basic real estate
+    ["no","No","NO"],
+    ["yes", "Yes", "ok", "okay", "Ok", "OK", "YES"],
+    ["buy","Buy","BUY","house to buy","home to buy","buy home","buy real estate"],
+    ["Condo","condo","House","house","Apartment","apartment","Co-op","co-op"],
+    ["email","communicate","contact"],
+    ["phone","Phone","employee", "Real human","real human","person"],
+    ["first name","Name","NAME"],
+    ["rent","Rent","RENT","lease"],
+    ["sell","Sell","SELL","property for sale","house for sale"],
+    ["joke","random joke","real estate joke"],
+    ["good morning","good afternoon", "good evening", "hi","hey","hello"], 
+    ["end","End","END","goodbye"], 
+    ["thank you","thanks","great"],
+    ["just browsing", "browsing"],
+    ["appointment", "consultation", "schedule","virtual appointment","virtual"],
+    ["office hour","office","address"],
+    ["open house"],
+];
+
+
+
